be16504f2f8ac5818229103942dbe2e687840c87
jahnavigummalla <jahnavigummalla1224@gmail.com>
2026-09-03 09:52:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (20.14%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x):
+        digit_sum = 0
+        n = x
+        while n > 0:
+            digit_sum += n % 10
+            n //= 10
+        if x % digit_sum == 0:
+            return digit_sum
+        return -1 
