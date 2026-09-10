91fa23309740fa742b2b3eca0e061af6ab4f5f4a
jahnavigummalla <jahnavigummalla1224@gmail.com>
2026-09-10 10:39:29 +0530

    Time: 55 ms (39.15%) | Memory: 39.7 MB (24.03%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkSubarraySum(self, nums, k):
+        r_map = {0: -1}
+        prefix = 0
+        for i in range (len(nums)):
+            prefix += nums[i]
+            rem = prefix % k
+            if rem in r_map:
+                if i - r_map[rem] >= 2:
+                    return True
+            else:
+                r_map[rem] = i
+        return False
